855c877cef0ab376b15904e542ee402acab63d56
Michael Wolf Hoffman <michaelhoffmanj@gmail.com>
2022-09-16 11:19:40 -0600

    working

--- index.js
+++ index.js
@@ -9,67 +9,58 @@ const createSubscriber = require("pg-listen");
     console.log("node.js server listening on port " + port);
   });
 
-  console.log(1);
   const dbClient = new Client({
-    host: "postgres",
-    user: "pguser",
-    password: "password",
-    database: "pg-notify-db",
-    port: "5432",
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    port: 5432,
   });
 
   await dbClient.connect();
 
-  // const tableName = "table_1";
-  // const notifyFnName = "number_changed_function";
-  // const triggerName = "number_changed_trigger";
-  // const eventName = "number_was_added";
+  const tableName = "table_1";
+  const notifyFnName = "number_changed_function";
+  const triggerName = "number_changed_trigger";
+  const eventName = "number_was_added";
 
-  // await dbClient.query(`CREATE TABLE IF NOT EXISTS ${tableName}(
-  //   number INT
-  //   )`);
+  await dbClient.query(`CREATE TABLE IF NOT EXISTS ${tableName}(
+    number INT
+    )`);
 
-  console.log(2);
+  await dbClient.query(
+    `CREATE OR REPLACE FUNCTION ${notifyFnName}()
+            RETURNS TRIGGER
+            LANGUAGE PLPGSQL
+            AS $$
+            DECLARE number text;
+            BEGIN
+            SELECT to_json(NEW.number)::text INTO number;
+            PERFORM pg_notify('${eventName}'::text, number);
+            RETURN NULL;
+            END;
+            $$`
+  );
 
-  // await dbClient.query(
-  //   `CREATE FUNCTION IF NOT EXISTS ${notifyFnName}()
-  //           RETURNS TRIGGER
-  //           LANGUAGE PLPGSQL
-  //           AS $$
-  //           DECLARE number text;
-  //           BEGIN
-  //           SELECT to_json(NEW.number)::text INTO number;
-  //           PERFORM pg_notify(${eventName}::text, number);
-  //           RETURN NULL;
-  //           END;
-  //           $$`
-  // );
+  await dbClient.query(
+    `CREATE TRIGGER ${triggerName} AFTER INSERT ON ${tableName}
+  FOR EACH ROW EXECUTE FUNCTION ${notifyFnName}()`
+  );
 
-  // await dbClient.query(
-  //   `CREATE TRIGGER  IF NOT EXISTS ${triggerName} AFTER INSERT ON dvs.cloud_hands_on_labs_v0_lab_content
-  // FOR EACH ROW EXECUTE FUNCTION ${notifyFnName}()`
-  // );
+  //  Create listener for db
+  const subscriber = createSubscriber({
+    connectionString: `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:5432/${process.env.PGDATABASE}`,
+  });
+  await subscriber.connect();
+  await subscriber.listenTo(eventName);
 
-  // // //  Create listener for db
-  // // const subscriber = createSubscriber({ connectionString: conString });
-  // // await subscriber.connect();
-  // // await subscriber.listenTo(eventName);
+  subscriber.notifications.on(eventName, async (number) => {
+    console.log(`${number} was added to the database`);
+  });
 
-  // // subscriber.notifications.on(eventName, async (number) => {
-  // //   console.log(`${number} was added to the database`);
-  // // });
+  //  Add Item to table
+  await dbClient.query(
+    `INSERT INTO ${tableName} (number) VALUES (${Math.round(Math.random())})`
+  );
 
-  // // //  Add Item to table
-  // // setInterval(async () => {
-  // //   await dbClient.query(`INSERT INTO ${tableName} (${Math.random()})  `);
-  // // }, 500);
-
-  // // //  Clean Up
-  // // setTimeout(async () => {
-  // //   await dbClient.query(`DROP TRIGGER IF EXISTS ${triggerName} ON ${tableName}`);
-  // //   await dbClient.query(`DROP FUNCTION IF EXISTS ${notifyFnName} ON ${tableName}`);
-  // //   await dbClient.query(`DELETE FROM ${tableName}`);
-  // //   await dbClient.query(`DROP TABLE FROM ${tableName}`);
-  // //   process.exit();
-  // // }, 10000);
 })();
